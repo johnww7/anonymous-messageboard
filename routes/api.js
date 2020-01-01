555066f26cc0c250d93d5dc831e1bfac531c1aa3
John Williams <johnnjit7@gmail.com>
2020-01-01 18:50:28 -0500

    Set up routes for api. js

--- routes/api.js
+++ routes/api.js
@@ -9,11 +9,39 @@
 'use strict';
 
 var expect = require('chai').expect;
+var MongoClient = require('mongodb');
+var ObjectId = require('mongodb').ObjectId;
+
+const CONNECTION_STRING = process.env.CONNECTION_STRING;
 
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board')
+    .get(function(req, res, next) {
+
+    })
+    .post(function(req, res, next) {
+
+    })
+    .put(function(req, res, next) {
+
+    })
+    .delete(function(req, res, next) {
+
+    });
     
-  app.route('/api/replies/:board');
+  app.route('/api/replies/:board')
+    .get(function(req, res, next) {
+
+    })
+    .post(function(req, res, next) {
+
+    })
+    .put(function(req, res, next) {
+
+    })
+    .delete(function(req, res, next) {
+
+    });
 
 };
