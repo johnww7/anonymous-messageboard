a0f9aae77a06ebdfd70a7ec29f40d803a4dc8a8b
John Williams <johnnjit7@gmail.com>
2020-01-08 18:34:05 -0500

    Working on mongoose schema and queries

---
+++ board_controller/thread.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+
+require('dotenv').config()
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+    keepAlive: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 500,
+    connectTimeoutMS: 35000,
+    socketTimeoutMS: 40000,
+    useNewUrlParser: true
+});
+
+let db = mongoose. connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function callback() {
+    console.log('Connected to database');
+});
+
+const replySchema = new mongoose.Schema({
+    _id: String,
+    text: String,
+    created_on: {type: Date, default: Date.now},
+    delete_password: String,
+    reported: Boolean
+});
+
+const threadSchema = new mongoose.Schema({
+    _id: String,
+    text: String,
+    created_on: {type: Date, default: Date.now},
+    bumped_on: {type: Date, default: Date.now},
+    reported: Boolean,
+    delete_password: String,
+    replies: {
+        type: [replySchema],
+        default: []
+    }
+});
+
+module.exports = mongoose.model('Thread', threadSchema);
+module.exports = mongoose.model('Replies', replySchema);
