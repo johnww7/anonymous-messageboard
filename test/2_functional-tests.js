067dcb31c8e017522246b484e4babac8c2d4780e
John Williams <johnnjit7@gmail.com>
2020-03-03 18:40:02 -0500

    Finished writing all function tests, just have to test them out

--- test/2_functional-tests.js
+++ test/2_functional-tests.js
@@ -61,7 +61,10 @@ suite('Functional Tests', function() {
         chai
           .request(server)
           .delete("/api/threads/general")
-          .send({_id:""})
+          .send({
+            thread_id: '',
+            delete_password: ''
+          })
           .end(function(err, res) {
             assert.equal(res.status, 200);
             assert.equal(res.body.result, 'success');
@@ -76,7 +79,7 @@ suite('Functional Tests', function() {
           .request(server)
           .put('/api/threads/general')
           .send({
-            _id: ''
+            thread_id: ''
           })
           .end(function(err, res) {
             assert.equal(res.status, 200);
@@ -92,19 +95,81 @@ suite('Functional Tests', function() {
   suite('API ROUTING FOR /api/replies/:board', function() {
     
     suite('POST', function() {
-      
+      test("Reply to a thread", function(done) {
+        chai
+          .request(server)
+          .post("/api/replies/general")
+          .send({
+            board: 'general',
+            thread_id: '5e584b1fa490a10dd24c441a',
+            text: 'A test reply',
+            delete_password: 'replyDelete',
+            reported: false
+          })
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.board, 'general');
+            assert.equal(res.body.text, 'A test reply');
+            assert.equal(res.body.delete_password, 'replyDelete');
+            done();
+          });
+      });
     });
     
     suite('GET', function() {
-      
+      test("Retrieve an entire thread", function(done) {
+        chai
+          .request(server)
+          .get("/api/replies/general")
+          .query({
+            thread_id: '5e584b1fa490a10dd24c441a'
+          })
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.isArray(res.body);
+            assert.notProperty(res.body[0], 'reported');
+            assert.notProperty(res.body[0], 'delete_password');
+            assert.property(res.body[0], 'board');
+            assert.property(res.body[0], '_id');
+            assert.property(res.body[0], 'replies');
+            done();
+          });
+      });
     });
     
     suite('PUT', function() {
-      
+      test('Report a reply', function(done) {
+        chai
+          .request(server)
+          .put('/api/replies/general')
+          .send({
+            thread_id: '',
+            reply_id: ''
+          })
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.body, 'success');
+            done();
+          });
+      });  
     });
     
     suite('DELETE', function() {
-      
+      test('Delete a post', function(done) {
+        chai
+          .request(server)
+          .delete("/api/replies/general")
+          .send({
+            thread_id: '',
+            reply_id: '',
+            delete_password: ''
+          })
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.result, 'success');
+            done();
+          });
+      });
     });
     
   });
